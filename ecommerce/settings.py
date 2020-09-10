fe3c04b2f23ad820a3d2873d8477798142c250e4
madhumaddy409 <itech.maddy.tm@gmail.com>
2020-09-10 19:43:23 +0530

    update1

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -122,9 +122,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-# STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
+
+
+
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
