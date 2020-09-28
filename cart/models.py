d9db03a6149b4e0235a668d36983e0fa6312b552
madhumaddy409 <itech.maddy.tm@gmail.com>
2020-09-28 16:33:38 +0530

    userid

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,5 @@
+
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
@@ -6,6 +8,7 @@ from users.models import Product
 
 class CartProd(models.Model):
     product_id = models.ForeignKey(Product, on_delete=models.CASCADE,default="")
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, default="")
     name = models.CharField(max_length=60, null=True, blank=True)
     category = models.CharField(max_length=60, null=True, blank=True)
     description = models.CharField(max_length=250, null=True, blank=True)
